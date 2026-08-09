56353c250d4acd1501b1e9e31f17c1e22490bfbb
karthik-ctrl25 <raikark318@gmail.com>
2026-08-09 16:25:46 +0530

    sorting with inbuilt feature 1

--- sortingarray/inbuiltsort.java
+++ sortingarray/inbuiltsort.java
@@ -3,7 +3,15 @@ import java.util.Arrays;
 public class inbuiltsort {
     public static void main(String[] args) {
         int arrc[]={5,4,1,3,2};
+        int arrp[]={7,5,1,3,2};
+        //basic sort 
         Arrays.sort(arrc);
-        System.out.print(Arrays.toString(arrc));
+        System.out.println(Arrays.toString(arrc));
+
+        //sorting from where we need 
+         Arrays.sort(arrp,0,3);
+        System.out.print(Arrays.toString(arrp));
+
+        
     }
 }
