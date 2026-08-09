3056e3b5d1eb2557f3c63bd89abe69d7cbd4f91f
karthik-ctrl25 <raikark318@gmail.com>
2026-08-09 16:22:46 +0530

    inbuilt 1st feature

---
+++ sortingarray/inbuiltsort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class inbuiltsort {
+    public static void main(String[] args) {
+        int arrc[]={5,4,1,3,2};
+        Arrays.sort(arrc);
+        System.out.print(Arrays.toString(arrc));
+    }
+}
