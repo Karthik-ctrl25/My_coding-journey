2acd58b9d63f05da20a4ce5839247dd1ea60c525
karthik-ctrl25 <raikark318@gmail.com>
2026-08-15 20:22:14 +0530

    sum of digonal elemnts by Optimized method

--- arrays_two/diagonalma.java
+++ arrays_two/diagonalma.java
@@ -4,16 +4,28 @@ public class diagonalma {
    public static int diagonalms(int matrix[][]){
 
     int sum=0;
-    // Brute force 
+    // // Brute force 
+    // for(int i=0;i<matrix.length;i++){
+    //     for(int j=0;j<matrix.length;j++){
+    //         //primary diagonal
+    //         if (i==j) {
+    //             sum+=matrix[i][j];
+    //         }
+    //         else if (i+j==matrix.length-1) {
+    //             sum+=matrix[i][j];
+    //         }
+    //     }
+    // }
+
+    //Optimized method
     for(int i=0;i<matrix.length;i++){
-        for(int j=0;j<matrix.length;j++){
-            //primary diagonal
-            if (i==j) {
-                sum+=matrix[i][j];
-            }
-            else if (i+j==matrix.length-1) {
-                sum+=matrix[i][j];
-            }
+        //primary diagonal
+
+        sum+=matrix[i][i];
+
+
+        if (i!=matrix.length-1-i) {
+            sum+=matrix[i][matrix.length-i-1];
         }
     }
     return sum;
