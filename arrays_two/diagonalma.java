d0bb6c0885b8106956c9d584c54eba09e3744c9f
karthik-ctrl25 <raikark318@gmail.com>
2026-08-15 20:11:03 +0530

    sum of digonal elemnts by brute force method

---
+++ arrays_two/diagonalma.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class diagonalma {
+   
+   public static int diagonalms(int matrix[][]){
+
+    int sum=0;
+    // Brute force 
+    for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix.length;j++){
+            //primary diagonal
+            if (i==j) {
+                sum+=matrix[i][j];
+            }
+            else if (i+j==matrix.length-1) {
+                sum+=matrix[i][j];
+            }
+        }
+    }
+    return sum;
+   }
+   public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                   {5,6,7,8},
+                   {9,10,11,12},
+                   {13,14,15,16}};
+                   
+                  System.out.println("the sum of diagonal elemnts is = "+ diagonalms(matrix));
+        
+    }
+}
