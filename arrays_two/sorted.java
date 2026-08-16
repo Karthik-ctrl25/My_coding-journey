a06ec50ad6ab79b24e25264911c258185ad76bbb
karthik-ctrl25 <raikark318@gmail.com>
2026-08-16 12:39:10 +0530

    the search in sorted array by stairs method

---
+++ arrays_two/sorted.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class sorted {
+
+    public static boolean searchingsort(int ma[][], int key){
+        int row=0;
+        int col=ma[0].length-1;
+        while (row<ma.length && col>=0) {
+            if (ma[row][col]==key) {
+                System.out.println("the number "+key+" is found at index ("+row+","+col+")");
+                return  true;
+            }
+            else if (key<ma[row][col]) {
+                //left side 
+                col--;
+            }
+            else{
+                //bottom 
+                row++;
+            }
+        }
+        System.out.println("the key is not found at MATRIX");
+        return false;
+    }
+    public static void main(String[] args) {
+        int ma[][]={{10,20,30,40},
+                    {15,25,35,45},
+                    {37,29,37,48},
+                    {32,33,39,50}};
+        int key=37;
+        searchingsort(ma, key);
+    }
+}
