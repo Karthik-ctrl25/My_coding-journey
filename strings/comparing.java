130634ef225e43df1269032a5ab9c86b94f50350
karthik-ctrl25 <raikark318@gmail.com>
2026-09-02 19:53:57 +0530

    the strings comparing function

---
+++ strings/comparing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class comparing{
+    public static void main(String[] args) {
+        String s1="tony";
+        String s2="tony";
+        if (s1.equals(s2)) {
+            System.out.println("the strings are equal");
+        }else{
+            System.out.println("strings are not equal");
+        }
+    }
+}
