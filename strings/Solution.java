c4a0484978641a294fd02c3d4ec61e84595d8560
karthik-ctrl25 <raikark318@gmail.com>
2026-09-23 19:25:43 +0530

    count vowels in word

---
+++ strings/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+public static void main(String args[]) {
+String str = "ShradhaDidi";
+String str1 = "ApnaCollege";
+String str2 = "ShradhaDidi";
+System.out.println(str.equals(str1) + " " + str.equals(str2));
+
+String str4 = "ApnaCollege".replace("l", "");
+System.out.println(str4);
+}
+}
